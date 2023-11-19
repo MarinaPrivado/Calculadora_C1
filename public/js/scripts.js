b1907293236516f2f9a148177a878c79121afdbc
Marina Luanda <luandapc3@gmail.com>
2023-11-19 14:45:50 -0300

    add function calcular

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,8 +8,21 @@ function clean(){
 function back(){
     
 }
-function calcular(){
-    
+function calcular (n1, operador, n2){
+        let result = '';
+        parseFloat(n1);
+        parseFloat(n2);
+        if (operador === '+') {
+          result = n1 + n2;
+        } else if (operador === '-') {
+          result =  n1 - n2;
+        } else if (operador === '*') {
+          result = n1 * n2;
+        } else if (operador === '/') {
+          result = n1 / n2;
+        }      
+        return result;
+      
 }
 
 module.exports = {insert, clean, back, calcular}
